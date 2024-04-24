f95375d9532b685d8540e8a22caa9946ef8d5fa6
Harshit <harshit2001pandey@gmail.com>
2024-04-24 10:00:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,15 @@
+
+
+function Card(){
+    return(
+        <>
+            <div className="card">
+                <img src="https://lh3.googleusercontent.com/a/ACg8ocL3OhhNexIhg3s4MsX_fpsktZePF7DGi0Yzpfa8OGMIgYPDwGqp=s288-c-no" alt="my_Pic" className="mypic"/>
+                <h2 className="card_name">It's me</h2>
+                <p className="card_info">My Name is Harshit Pandey, and i love Gaming</p>
+            </div>
+        </>
+    );
+}
+
+export default Card;
